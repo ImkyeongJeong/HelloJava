9f809b312491c6170155593faf393abae12f2a14
imkye <imkye@JIK>
2022-04-13 00:28:41 +0900

    중첩 반복문(구구단, * 출력)

---
+++ P20220412/src/co/edu/loop/NestedExample.java
@@ -0,0 +1,35 @@
+package co.edu.loop;
+
+public class NestedExample {
+
+	public static void main(String[] args) {
+		//중첩 반복문 문제
+		
+		//구구단 (2단 ~ 9단) ex) 2 * 1 = 2
+
+
+		// *
+		// **
+		// ***
+		// ****
+		// *****
+		
+		
+		// *****
+		// ****
+		// ***
+		// **
+		// *
+		
+		
+		
+		//     *
+		//    **
+		//   ***
+		//  ****
+		// *****
+		
+		
+	}
+
+}
